4af7dcd624106e84eefd8257ea6bf07dc31c0195
daianaadepaula <daianaadepaula1@gmail.com>
2025-02-26 04:35:07 -0300

    feat: create invite

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,12 +1,11 @@
 import { IconButton } from '@/components/icon-button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
-import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from 'lucide-react'
+import { Copy, Link } from 'lucide-react'
 import Image from 'next/image'
 
 import logo from '../../assets/logo.svg'
-import cooper from '../../assets/medal-cooper.svg'
-import gold from '../../assets/medal-gold.svg'
-import silver from '../../assets/medal-silver.svg'
+import { Ranking } from './ranking'
+import { Stats } from './stats'
 
 export default function InvitePage() {
   return (
@@ -48,73 +47,13 @@ export default function InvitePage() {
             </IconButton>
           </InputRoot>
 
-          <div className="grid gap-3 md:grid-cols-3">
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1089
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Acessos ao link
-              </span>
-              <MousePointerClick className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1089
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Inscrições feitas
-              </span>
-              <BadgeCheck className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                3º
-              </span>
-              <span className="text-sm text-gray-300 leading-none text-center">
-                Posição do ranking
-              </span>
-              <Medal className="size-5 text-purple absolute top-3 left-3" />
-            </div>
-          </div>
+          {/* stats */}
+          <Stats />
         </div>
       </div>
-      {/* ranking */}
-      <div className="w-full max-w-[440px] space-y-5">
-        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-          Ranking de indicações
-        </h2>
 
-        <div className="space-y-4">
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1ª</span> | Daiana de Paula
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              2087
-            </span>
-            <Image src={gold} alt="" className="absolute top-0 right-8" />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">2ª</span> | Maria Araujo
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1887
-            </span>
-            <Image src={silver} alt="" className="absolute top-0 right-8" />
-          </div>
-          <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">3ª</span> | Djalma Olegário
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              987
-            </span>
-            <Image src={cooper} alt="" className="absolute top-0 right-8" />
-          </div>
-        </div>
-      </div>
+      {/* ranking */}
+      <Ranking />
     </div>
   )
 }
